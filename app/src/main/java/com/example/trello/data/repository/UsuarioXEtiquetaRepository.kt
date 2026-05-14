b1d00701b17cd079e20d610946fde9e69d00a953
Pablo Arauz Caballero <128090351+PabloArauzCaballero@users.noreply.github.com>
2026-05-14 19:38:49 -0400

    inyeccion dependencias

--- app/src/main/java/com/example/trello/data/repository/UsuarioXEtiquetaRepository.kt
+++ app/src/main/java/com/example/trello/data/repository/UsuarioXEtiquetaRepository.kt
@@ -1,9 +1,44 @@
 package com.example.trello.data.repository
 import com.example.trello.data.database.AppDatabase
+import com.example.trello.data.entities.EtiquetaXTarea
 import javax.inject.Inject
 
 class UsuarioXEtiquetaRepository @Inject constructor(
     private val db: AppDatabase
 ) {
+    suspend fun asociarEtiquetaATarea(etiquetaXTarea: EtiquetaXTarea): Long?{
+        return try {
+            db
+                .etiquetaXTareaDao()
+                .asociarEtiquetaATarea(etiquetaXTarea = etiquetaXTarea)
+        }catch (e: Exception){
+            e.printStackTrace()
+            null
+        }
+    }
 
+    suspend fun quitarTodasLasEtiquetasDeTarea(idTarea: Int): Int?{
+        return try {
+            db
+                .etiquetaXTareaDao()
+                .quitarTodasLasEtiquetasDeTarea(idTarea = idTarea)
+        }catch (e: Exception){
+            e.printStackTrace()
+            null
+        }
+    }
+
+    suspend fun quitarEtiquetaTarea(
+        idTarea: Int,
+        idEtiqueta: Int,
+    ): Int?{
+        return try {
+            db
+                .etiquetaXTareaDao()
+                .quitarEtiquetaDeTarea(idTarea = idTarea, idEtiqueta = idEtiqueta)
+        }catch (e: Exception){
+            e.printStackTrace()
+            null
+        }
+    }
 }
